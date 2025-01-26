36fe9b5519b7a0e897e15abd8059c6cddc3bd6d0
BlimeKoop <blimekoop@gmail.com>
2025-01-26 14:01:32 +1030

    SoundManager uses Audio Mixer Groups (SFX and Music)

---
+++ Pirate Jam 16 Game/Assets/Scripts/Components/ManagerReferences.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ManagerReferences : MonoBehaviour
+{
+    [Header("SoundManager")]
+    public AudioMixerGroup sfxMixerGroup;
+    public AudioMixerGroup musicMixerGroup;
+
+    public GameObject nonPositionalSource { get; private set; }
+
+    private void Awake()
+    {
+        SoundManager.References = this;
+
+        nonPositionalSource = new GameObject("Non Positional Sound Sources");
+    }
+}
