10ee35178f67871db0d8709bed75fe018ae92cac
Roomies-entertainment <roomiesentertainment@gmail.com>
2025-01-27 12:20:13 +1100

    Main Game level Editing
    Added Player UI to scene
    
    - Adjusted Player UI to have gradient health on player

--- Pirate Jam 16 Game/Assets/Scripts/PlayerUI.cs
+++ Pirate Jam 16 Game/Assets/Scripts/PlayerUI.cs
@@ -7,8 +7,13 @@ public class PlayerUI : MonoBehaviour
     [Header("")]
     [SerializeField] private Slider HealthBar;
 
+    [SerializeField] private Image fill;
+
+    [SerializeField] private Gradient gradient;
+
     public void UpdateHealthBar(float health01, DetectionData data = null)
     {
         HealthBar.value = health01;
+        fill.color = gradient.Evaluate(HealthBar.normalizedValue);
     }
 }
