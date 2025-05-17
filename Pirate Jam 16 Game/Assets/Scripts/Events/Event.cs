153e02eba1c58880a8f20afba048a1dd61ca0072
BlimeKoop <blimekoop@gmail.com>
2025-05-18 03:16:41 +0930

    Event code fixed & extended

---
+++ Pirate Jam 16 Game/Assets/Scripts/Events/Event.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class EventData
+{
+    public UnityEvent Event;
+    public float delayMin;
+    public float delayMax;
+}
+
+public class Event : MonoBehaviour
+{
+    [SerializeField] private bool loop;
+
+    [SerializeField] private EventData _event;
+    
+    private float delay;
+    private float timer;
+
+    private void OnEnable()
+    {
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+        if (timer >= delay)
+        {
+            _event.Event?.Invoke();
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
