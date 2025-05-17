c0346a7aa38d335d2f746fb5752c1a16387209d6
BlimeKoop <blimekoop@gmail.com>
2025-05-18 02:16:44 +0930

    Object spawn script extended

--- Pirate Jam 16 Game/Assets/Scripts/RandomM.cs
+++ Pirate Jam 16 Game/Assets/Scripts/RandomM.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public static class RandomM
 {
     public static System.Random RandomSeedless { get; private set; } = new();
-    
+
     public static float Float0To1(System.Random random = null)
     {
         if (random == null)
             random = RandomSeedless;
 
-        return (float) random.NextDouble();
+        return (float)random.NextDouble();
     }
+
     public static float Range(float min, float max, System.Random random = null)
     {
         if (random == null)
             random = RandomSeedless;
 
         return Mathf.Lerp(min, max, (float) random.NextDouble());
+    }
+
+    public static Vector2 RandomDirection(System.Random random = null)
+    {
+        if (random == null)
+            random = RandomSeedless;
+
+        float angle = (float) (random.NextDouble() * 2 * Mathf.PI);
+        
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
     }
 }
