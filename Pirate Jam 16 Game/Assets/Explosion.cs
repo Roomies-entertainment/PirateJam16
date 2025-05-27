b1274485243b68223e1b6aa008fe7921d34a803e
Roomies-entertainment <roomiesentertainment@gmail.com>
2025-05-27 21:13:08 +1000

    Added BOOM

---
+++ Pirate Jam 16 Game/Assets/Explosion.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float fieldofImpact;
+
+    public float force;
+
+    public void Explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact);
+
+        foreach (Collider2D obj in objects)
+        {
+            Rigidbody2D rb = obj.GetComponentInParent<Rigidbody2D>();
+
+            print(rb);
+
+            if (rb == null)
+                continue;
+
+            Vector2 direction = obj.transform.position - transform.position;
+
+            rb.AddForce(direction * force, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldofImpact);
+    }
+}
